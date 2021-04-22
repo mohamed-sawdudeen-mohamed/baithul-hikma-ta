fc2a619521e1c917f2739aad2fb3f51dee4c8be1
Mohamed_Sawdudeen <sawdudeenm@gmail.com>
2021-04-22 23:22:54 +0530

    all-pages

---
+++ index.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Baithul Hikma</title>
+    <link rel="stylesheet" href="./assets/css/styles.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Kavivanar&display=swap" rel="stylesheet">  
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+</head>
+<body>
+    <header class="baner">
+        <h1>பைதுல் ஹிக்மா <br />பெண்கள் அரபுக் கல்லூரி</h1>
+        <img class="logo" src="./assets/images/logo.png" alt="bythul hikma logo">
+        <h1>Baithul Hikma <br />Ladies Arabic College</h1>
+    </header>
+
+    <section class="lead">
+        <div class="bg-color">
+            <nav class="main-nav not-white">
+                <ul class="nav">
+                    <li class="nav-item"><a href="#" class="nav-links">முகப்பு</a></li>
+                    <li class="nav-item"><a href="#" class="nav-links">எம்மைப் பற்றி</a></li>
+                    <li class="nav-item"><a href="#" class="nav-links">மாணவியர் பகுதி</a></li>
+                    <li class="nav-item"><a href="#" class="nav-links">மல்டிமீடியா</a></li>
+                    <li class="nav-item"><a href="#" class="nav-links">தொடர்பு கொள்ள</a></li>
+                </ul>
+            </nav>
+            <div class="welcome-lead">
+                <p class="welcome-title">
+                    WELCOME
+                </p>
+                <a href="#">எம்மைப் பற்றி</a>
+            </div>
+        </div>
+    </section>
+
+    <section class="vistion">
+        <div>
+            <h2>தூர நோக்கு</h2>
+            <p>“படைப்பாற்றல் மற்றும் புதுமைகளைத் தூண்டும் ஒரு கல்விச் சூழலை வழங்கி, சமூகத்தின் கலாச்சாரத்தை மேம்படுத்துவதற்கும், நேர் மறையான சிந்தனையை பரப்புவதற்கும், ஒரு தனித்துவமான கலாச்சார பாரம்பரியத்தை வேரூன்றச் செய்வதற்கும் திறன் கொண்ட பெண் பணியாளர்களை உருவாக்கல்.  “
+            </p>
+        </div>      
+    </section>
+
+    <section class="stats">
+        <div>
+            <h2>எண்ணிக்கையில் எமது கல்லூரி</h2>
+            <h3>எமது கல்லூரியின் பணி அளவைக் காட்டும் புள்ளிவிபரங்கள்</h3>
+            <div class="stats-cricle-container">
+                <div>
+                    <i class="fa fa-user-plus icon"></i>    
+                    <a href="#"><div class="circle">15</div></a>
+                    <p>புதிய மாணவிகள்</p>
+                </div>
+                <div>
+                    <i class="fa fa-users icon"></i>
+                    <a href="#"><div class="circle">55</div></a>
+                    <p>மொத்த மாணவிகள்</p>
+                </div>
+                <div>
+                    <i class="fa fa-user-graduate icon"></i>
+                    <a href="#"><div class="circle">60</div></a>
+                    <p>பட்டம் பெற்றோர்</p>
+                </div>
+                <div>
+                    <i class="fa fa-chalkboard-teacher icon"></i>
+                    <a href="#"><div class="circle">20</div></a>
+                    <p>ஆசிரியர்கள்</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="info">
+        <div class="bg-color">
+            <a href="#">
+                <div class="info-item">
+                    <div><i class='fa fa-calendar-alt'></i></div>
+                    <div><span>கல்வியாண்டுக் கலன்டர்</span></div>
+                    
+                </div>
+            </a>
+        
+            <a href="#">
+                <div class="info-item">
+                    <div><i class='fa fa-clipboard-check'></i></div>
+                    <div><span>புதிய மாணவர் அனுமதி</span></div>
+                </div>
+            </a>
+            
+            <a href="#">
+                <div class="info-item">
+                    <div><i class='fa fa-phone-square-alt'></i></div>
+                    <div><span>தொடர்பு கொள்ள</span></div>
+                </div>
+            </a>
+    
+            <a href="#">
+                <div class="info-item">
+                    <div><i class='fa fa-map-marker-alt'></i></div>
+                    <div><span>வரைபடம்</span></div>
+                </div>
+            </a>
+        </div>
+    </section>
+
+
+
+    <section id="quote-slider" class="qoute-slider" slide-length="3" active-slide="1">
+        <div id="slide-1" class="slide active" style="background-image: url(./assets/images/lead-image.jpg);">
+            
+        </div>
+        <div id="slide-2" class="slide" style="background-image: url(./assets/images/img-1.jpg);">
+            
+        </div>
+        <div id="slide-3" class="slide" style="background-image: url(./assets/images/col-1.jpg);">
+            
+        </div>
+
+        <div id="slider-prev-btn" class="action-btn prev">
+            <i class="fa fa-chevron-left"></i>
+        </div>
+        <div id="slider-next-btn" class="action-btn next">
+            <i class="fa fa-chevron-right"></i>
+        </div>
+
+        <div class="indicators">
+            <div id="slider-indicator-1" move-slider-id="1" class="active"></div>
+            <div id="slider-indicator-2" move-slider-id="2"></div>
+            <div id="slider-indicator-3" move-slider-id="3"></div>
+        </div>
+    </section>
+
+    <section class="media">
+        <h2>Media Section...</h2>
+        <div id="main-media-slider" class="media-container">
+            <div class="media-item">
+                <iframe width="100%" height="100%" src="https://www.youtube.com/embed/_8ylWl9zUiY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <div class="media-item-text">
+                    <h6>Hello World 2021</h6>
+                    <p>2021-03-10 21:55</p>
+                </div>
+            </div>
+            <div class="media-item">
+                <iframe width="100%" height="100%" src="https://www.youtube.com/embed/58eU4bnYsRg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <div class="media-item-text">Hello World 2021</div>
+            </div>
+            <div class="media-item">
+                <iframe width="100%" height="100%" src="https://www.youtube.com/embed/OiE1p2Sw8zY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <div class="media-item-text">
+                    <h5>Hello World 2021</h5>
+                    <h6>2021-03-10 21:55</h6>
+                </div>
+            </div>
+            <div class="media-item">
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/r3NFwDRsS3Y" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div class="media-item">5</div>
+            <div class="media-item">6</div>
+            <div class="media-item">7</div>
+            <div class="media-item">8</div>
+            <div class="media-item">9</div>
+            <div class="media-item">10</div>
+        </div>   
+    </section>
+
+    <section id="quote-artical" class="artical">
+        <div class="artical-head">
+            <h2>ஆக்கங்கள் & கட்டுரைகள்</h2>
+        </div>
+
+        
+
+        <div class="article-btn">
+            <div id="article-prev-btn" class="article-btn prev">
+                <i class="fa fa-chevron-left"></i>
+            </div>
+            <div id="article-next-btn" class="article-btn next">
+                <i class="fa fa-chevron-right"></i>
+            </div>
+        </div>
+
+        <div class="article-slides">
+            <div class="article">
+                <img src="./assets/images/img-5.jpg"  alt="">
+                <h3>கல்வியின் முக்கியத்துவம்</h3>
+            </div>
+
+            <div class="article">
+                <img src="./assets/images/img-5.jpg" alt="">
+                <h3>இறைவன் எங்கே உள்ளான்?</h3>
+            </div>
+            
+            <div class="article">
+                <img src="./assets/images/img-5.jpg" alt="">
+                <h3>யார் இந்த ஷPஆக்கள்</h3>
+            </div>
+
+            <div class="article">
+                <img src="./assets/images/img-5.jpg"  alt="">
+                <h3>கல்வியின் முக்கியத்துவம்</h3>
+            </div>
+
+            <div class="article">
+                <img src="./assets/images/img-5.jpg" alt="">
+                <h3>இறைவன் எங்கே உள்ளான்?</h3>
+            </div>
+            
+            <div class="article">
+                <img src="./assets/images/img-5.jpg" alt="">
+                <h3>யார் இந்த ஷPஆக்கள்</h3>
+            </div>
+
+            <div class="article">
+                <img src="./assets/images/img-5.jpg"  alt="">
+                <h3>கல்வியின் முக்கியத்துவம்</h3>
+            </div>
+
+            <div class="article">
+                <img src="./assets/images/img-5.jpg" alt="">
+                <h3>இறைவன் எங்கே உள்ளான்?</h3>
+            </div>
+            
+            <div class="article">
+                <img src="./assets/images/img-5.jpg" alt="">
+                <h3>யார் இந்த ஷPஆக்கள்</h3>
+            </div>
+
+            <div class="article">
+                <img src="./assets/images/img-5.jpg"  alt="">
+                <h3>கல்வியின் முக்கியத்துவம்</h3>
+            </div>
+
+            <div class="article">
+                <img src="./assets/images/img-5.jpg" alt="">
+                <h3>இறைவன் எங்கே உள்ளான்?</h3>
+            </div>
+            
+            <div class="article">
+                <img src="./assets/images/img-5.jpg" alt="">
+                <h3>யார் இந்த ஷPஆக்கள்</h3>
+            </div>
+        </div>
+    </section>
+
+      
+
+    <footer>
+        <section id="contact">
+            
+            <div class="social-links-container">
+                <ul class="social-links">
+                    <li><a href="#">FACEBOOK</a></li>
+                    <li><a href="#">TWITTER</a></li>
+                    <li><a href="#">INSTAGRAM</a></li>
+                    <li><a href="#">LINKEDIN</a></li>
+                </ul>
+            </div>
+            
+            <div class="page-wrap">
+                <a href="#" id="send-email" class="button"><i class="fa fa-paper-plane" aria-hidden="true" style="margin-right: 20px;"></i>Get In Touch</a>
+                <p>
+                Baithul Hikma Ladies Arabic College<br>
+                <i class="fas fa-map-marker-alt"></i> 126/01, Himbutuduwawatte, Ethiligoda, Galle, Sri Lanka <br>
+                <i class="fas fa-phone-square-alt"></i> Phone: 091-3781690  <br>
+                <i class="fas fa-envelope-open-text"></i> Email: baithulhikma2002@gmail.com
+                </p>
+
+                <h6>baithulhikma.org All rights reserved</h6>
+            </div>     
+        </section>
+    </footer>
+    <script src="./assets/js/scripts.js"></script>    
+</body>
+</html>
+
+
+
+
